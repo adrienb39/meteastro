6d1f6d4c9ff95db0adf77c5eeb5c90e7ee31c20c
adrienb39 <adrienb39@yahoo.com>
2024-10-25 19:08:05 +0200

    fix: update config BDD

--- import_dans_le_php/footer.php
+++ import_dans_le_php/footer.php
@@ -27,8 +27,8 @@
         <p class="footer_block">
             Site Web créé le : <b>Vendredi 8 Avril 2022</b><br>
             Dernière mise à jour du Site Web : <b>Vendredi 5 Avril 2024</b><br>
-            Version du Site Web : <b>1.4.2</b><br>
-            <?php include "counter.php"; ?>
+            Version du Site Web : <b>1.6</b><br>
+            <?php // include "counter.php"; ?>
         </p>
 
         <div class="switch" id="switch">
