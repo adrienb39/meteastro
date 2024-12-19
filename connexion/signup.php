d2aaf80806cb39a8d7f2fd747a24149ee973e98d
adrienb39 <adrienb39@yahoo.com>
2024-12-19 20:49:31 +0100

    mise à jour de l'interface complet du formulaire de connexion / inscription

--- connexion/signup.php
+++ connexion/signup.php
@@ -8,71 +8,15 @@ require_once "../config/controllerUserData.php";
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
     <link rel="stylesheet" href="connexion.css" />
     <link rel="stylesheet" href="information/information.css" />
     <title>Meteastro : Astronomie / meteorologie</title>
-
-    <style>
-    /* Ciel étoilé animé */
-    #stars-background {
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        pointer-events: none;
-        z-index: -1;
-    }
-
-    /* Créer des étoiles animées */
-    .star {
-        position: absolute;
-        border-radius: 50%;
-        background-color: white;
-        animation: twinkle 1.5s infinite ease-in-out;
-    }
-
-    /* Positionner aléatoirement les étoiles */
-    #stars-background .star:nth-child(1) { top: 20%; left: 10%; width: 2px; height: 2px; animation-duration: 2s; }
-    #stars-background .star:nth-child(2) { top: 50%; left: 30%; width: 3px; height: 3px; animation-duration: 2.5s; }
-    #stars-background .star:nth-child(3) { top: 80%; left: 70%; width: 1px; height: 1px; animation-duration: 1.2s; }
-    #stars-background .star:nth-child(4) { top: 40%; left: 90%; width: 2px; height: 2px; animation-duration: 1.7s; }
-    #stars-background .star:nth-child(5) { top: 60%; left: 20%; width: 4px; height: 4px; animation-duration: 3s; }
-
-    /* Animation pour le scintillement des étoiles */
-    @keyframes twinkle {
-        0% { opacity: 0.7; }
-        50% { opacity: 1; }
-        100% { opacity: 0.7; }
-    }
-
-    /* Styles pour la bannière */
-    #cookie-banner p {
-        margin: 5px 0;
-    }
-
-    #cookie-banner a {
-        color: #fff;
-        text-decoration: underline;
-    }
-
-    #cookie-banner button {
-        background-color: #555;
-        border: none;
-        padding: 8px 16px;
-        color: #fff;
-        cursor: pointer;
-        margin-top: 10px;
-    }
-
-    #cookie-banner button:hover {
-        background-color: #444;
-    }
-</style>
 </head>
 
 <body>
+    <div class="star-field"></div>
+    <div class="glowing-stars"></div>
     <?php if (isset($_SESSION['email']) && isset($_SESSION['password'])) { ?>
         <div class="container">
             <div class="forms-container">
@@ -113,14 +57,20 @@ require_once "../config/controllerUserData.php";
                         </div>
                         <div>
                             <input type="checkbox" name="consent" id="consent" required />
-                            <label for="consent">J'accepte les <a href="terms.php" target="_blank">termes et
-                                    conditions</a>.</label>
+                            <label for="consent">J'accepte les <a style="text-decoration: none; color: red;"
+                                    href="terms.php" target="_blank">termes et conditions</a>.</label>
                         </div>
                         <input type="submit" name="signup" value="S'inscrire" class="box-button btn" />
+                        <div style="display: block ruby;">
+                            <h3>Déjà inscrit ? </h3>
+                            <a style="text-decoration: none; color: red;" class="btn transparent" href="login.php">
+                                CONNEXION
+                            </a>
+                        </div>
                         <?php
                         if (count($errors) == 1) {
                             ?>
-                            <div style="color: red; text-align: center; padding: 0 100px;">
+                            <div style="color: red; text-align: center;">
                                 <?php
                                 foreach ($errors as $showerror) {
                                     echo htmlspecialchars($showerror);
@@ -131,7 +81,7 @@ require_once "../config/controllerUserData.php";
                             <?php
                         } elseif (count($errors) > 1) {
                             ?>
-                            <div style="color: red; text-align: center; padding: 0 100px;">
+                            <div style="color: red; text-align: center;">
                                 <ul>
                                     <?php
                                     foreach ($errors as $showerror) {
@@ -151,30 +101,12 @@ require_once "../config/controllerUserData.php";
                     </form>
                 </div>
             </div>
-
-            <div class="panels-container">
-                <div class="panel left-panel">
-                    <div class="content">
-                        <h3>Déjà inscrit ?</h3>
-                        <p>
-                            Si vous êtes déjà inscrit, vous pouvez vous connecter pour accéder aux fonctionnalités et aux
-                            contenus supplémentaires !
-                        </p>
-                        <a class="btn transparent" href="login.php">
-                            CONNEXION
-                        </a>
-                    </div>
-                    <img src="https://i.ibb.co/nP8H853/Mobile-login-rafiki.png" class="image" alt="" />
-                </div>
-            </div>
         </div>
     <?php } ?>
-    <div id="cookie-banner"
-        style="position: fixed; bottom: 0; width: 100%; background-color: #333; color: #fff; padding: 10px; text-align: center; z-index: 1000;">
-        <div id="stars-background"></div>
-        <p>Une nouvelle version majeure (version 4) de la page de connexion/inscription sera bientôt disponible. Restez
-            à l'affût !</p>
-    </div>
+    <div class="planet"></div>
+    <div class="asteroid"></div>
+
+    <?php include "about.php"; ?>
 
     <script src="login.js"></script>
 
