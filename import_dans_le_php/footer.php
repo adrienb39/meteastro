e8430b29862c54e6bb6a4ed7cececf7d0c338325
adrienb39 <adrienb39@yahoo.com>
2025-01-04 16:50:24 +0100

    correction de la page pour ajouter du contenu

--- import_dans_le_php/footer.php
+++ import_dans_le_php/footer.php
@@ -26,8 +26,8 @@
         </p>
         <p class="footer_block">
             Site Web créé le : <b>Vendredi 8 Avril 2022</b><br>
-            Dernière mise à jour du Site Web : <b>Vendredi 20 Décembre 2024</b><br>
-            Version du Site Web : <b>1.8</b><br>
+            Dernière mise à jour du Site Web : <b>Samedi 4 Janvier 2025</b><br>
+            Version du Site Web : <b>1.9</b><br>
             <?php include "counter.php"; ?>
         </p>
 
