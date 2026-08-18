d2b8b75650db7c761b83b17106ef61c11f72e32b
adrienb39 <adrienb39@yahoo.com>
2026-08-18 18:54:28 +0200

    ajout des newslatters

--- __partials/counter.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once __DIR__ . '/counter/counter.php';
-
-// add IP address if it doesn't exist in list
-addUniqueIP();
-
-// print unique visitors
-echo "Visiteurs : " . getUniqueVisitor();
